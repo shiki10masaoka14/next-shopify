404a20f77ca90bb8bea5def094e834fc7e2cc7d1
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2022-01-19 04:53:27 +0000

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
-import Link from "next/link";
-import { memo, VFC } from "react";
-
-const Login: VFC = memo(() => {
-  return (
-    <>
-      <Link href={"/practice"}>practice</Link>
-    </>
+import { request, gql } from "graphql-request";
+export async function getServerSideProps() {
+  const query = gql`
+    {
+      frameworks {
+        id
+        name
+      }
+    }
+  `;
+  const data = await request(
+    "http://localhost:3000/api/graphql",
+    query,
   );
-});
-Login.displayName = "Login";
-
-export default Login;
+  const { frameworks } = data;
+  return {
+    props: {
+      frameworks,
+    },
+  };
+}
+export default function Home({ frameworks }) {
+  return (
+    <div>
+      <ul>
+        {frameworks.map((f) => (
+          <li key={f.id}>{f.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
