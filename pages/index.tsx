5c6c5433ac14193f1ee7fb77b5b615993fd2e58f
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2022-01-25 11:28:21 +0000

    fauna

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,8 @@
-import { request, gql } from "graphql-request";
-export async function getServerSideProps() {
-  const query = gql`
-    {
-      frameworks {
-        id
-        name
-      }
-    }
-  `;
-  const data = await request(
-    "http://localhost:3000/api/graphql",
-    query,
-  );
-  const { frameworks } = data;
-  return {
-    props: {
-      frameworks,
-    },
-  };
-}
-export default function Home({ frameworks }) {
-  return (
-    <div>
-      <ul>
-        {frameworks.map((f) => (
-          <li key={f.id}>{f.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+import { memo, VFC } from "react";
+
+const home: VFC = memo(() => {
+  return <>home page</>;
+});
+home.displayName = "home";
+
+export default home;
