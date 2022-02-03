9f6696173c8b49b865dc36b6dc884ad8f0c91c77
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2022-02-03 06:35:59 +0000

    11

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { RecoilRoot } from "recoil";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -10,11 +9,9 @@ const App = ({ Component, pageProps }: AppProps) => {
         <title>ユーザーマネージメント</title>
         <link rel="icon" href="favicon.svg" />
       </Head>
-      <RecoilRoot>
-        <ChakraProvider>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </RecoilRoot>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </>
   );
 };
--- recoil/recoilAllTodos.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const recoilAllTodos = atom({
-  key: "recoilAllTodos",
-  default: undefined,
-});
