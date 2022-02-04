71b50767b59611f61a5cb94502d74c6addf3d1b8
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2022-02-04 05:43:02 +0000

    auth adapter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,23 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { SessionProvider as AuthProvider } from "next-auth/react";
 import Head from "next/head";
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = (
+  { Component, pageProps }: AppProps,
+  session,
+) => {
   return (
     <>
       <Head>
         <title>ユーザーマネージメント</title>
         <link rel="icon" href="favicon.svg" />
       </Head>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <AuthProvider session={session}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </AuthProvider>
     </>
   );
 };
---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import { Client as FaunaClient } from "faunadb";
+import { FaunaAdapter } from "@next-auth/fauna-adapter";
+
+const client = new FaunaClient({
+  secret: process.env.FAUNA_SECRET_KEY,
+});
+
+export default NextAuth({
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+      maxAge: 10 * 60, // 10 minutes
+    }),
+  ],
+  adapter: FaunaAdapter(client),
+});
