b1ee74c63919c349d81028aad4518e606d8629a0
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2022-02-07 11:45:05 +0000

    deploy7

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
-import { Link, Text } from "@chakra-ui/react";
+import {
+  Center,
+  Container,
+  Link,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { GetServerSideProps, NextPage } from "next";
 import { Session } from "next-auth";
 import { getSession, useSession } from "next-auth/react";
 import NextLink from "next/link";
 import { memo } from "react";
-import { graphQLClient } from "../utils/fetcher";
-import {
-  FindTodoByIdQuery,
-  getSdk,
-} from "../utils/generated";
+import { FindTodoByIdQuery } from "../utils/generated";
 
 // ここまで「import」
 //
@@ -16,34 +18,35 @@ import {
 //
 // ここから
 
-const home: NextPage<FindTodoByIdQuery> = memo(
-  ({ findTodoByID }) => {
-    const { data: session, status } = useSession();
-    const loading = status === "loading";
+const home: NextPage<FindTodoByIdQuery> = memo(() => {
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
 
-    return (
-      <>
-        <NextLink href="/auth/signin" passHref>
-          <Link>go to sign in page</Link>
-        </NextLink>
-        {loading ? (
-          <Text>Loading...</Text>
-        ) : !session ? (
-          <Text>Log in</Text>
-        ) : (
-          <>
-            <Text>{session?.user?.email}</Text>
-            <NextLink href="/todo" passHref>
-              <Link>go to todo page</Link>
-            </NextLink>
-            <br />
-          </>
-        )}
-        <Text>{findTodoByID?.task}</Text>
-      </>
-    );
-  },
-);
+  return (
+    <Container>
+      <Center minH={"100vh"}>
+        <VStack>
+          <NextLink href="/auth/signin" passHref>
+            <Link>go to sign in page</Link>
+          </NextLink>
+          {loading ? (
+            <Text>Loading...</Text>
+          ) : !session ? (
+            <Text>Log in</Text>
+          ) : (
+            <>
+              <Text>{session?.user?.email}</Text>
+              <NextLink href="/todo" passHref>
+                <Link>go to todo page</Link>
+              </NextLink>
+              <br />
+            </>
+          )}
+        </VStack>
+      </Center>
+    </Container>
+  );
+});
 home.displayName = "home";
 
 export default home;
@@ -51,15 +54,9 @@ export default home;
 export const getServerSideProps: GetServerSideProps<{
   session: Session | null;
 }> = async (context) => {
-  const sdk = getSdk(graphQLClient);
-  const { findTodoByID } = await sdk.FindTodoById({
-    id: "322845040814588499",
-  });
-
   return {
     props: {
       session: await getSession(context),
-      findTodoByID,
     },
   };
 };
