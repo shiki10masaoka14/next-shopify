9dd58b573c25c14b2fc66d42fdf9f26d92857130
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2022-02-07 11:05:52 +0000

    deploy5

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,11 @@ import { Session } from "next-auth";
 import { getSession, useSession } from "next-auth/react";
 import NextLink from "next/link";
 import { memo } from "react";
+import { graphQLClient } from "../utils/fetcher";
+import {
+  FindTodoByIdQuery,
+  getSdk,
+} from "../utils/generated";
 
 // ここまで「import」
 //
@@ -11,31 +16,34 @@ import { memo } from "react";
 //
 // ここから
 
-const home: NextPage = memo(() => {
-  const { data: session, status } = useSession();
-  const loading = status === "loading";
+const home: NextPage<FindTodoByIdQuery> = memo(
+  ({ findTodoByID }) => {
+    const { data: session, status } = useSession();
+    const loading = status === "loading";
 
-  return (
-    <>
-      <NextLink href="/auth/signin" passHref>
-        <Link>go to sign in page</Link>
-      </NextLink>
-      {loading ? (
-        <Text>Loading...</Text>
-      ) : !session ? (
-        <Text>Log in</Text>
-      ) : (
-        <>
-          <Text>{session?.user?.email}</Text>
-          <NextLink href="/todo" passHref>
-            <Link>go to todo page</Link>
-          </NextLink>
-          <br />
-        </>
-      )}
-    </>
-  );
-});
+    return (
+      <>
+        <NextLink href="/auth/signin" passHref>
+          <Link>go to sign in page</Link>
+        </NextLink>
+        {loading ? (
+          <Text>Loading...</Text>
+        ) : !session ? (
+          <Text>Log in</Text>
+        ) : (
+          <>
+            <Text>{session?.user?.email}</Text>
+            <NextLink href="/todo" passHref>
+              <Link>go to todo page</Link>
+            </NextLink>
+            <br />
+          </>
+        )}
+        <Text>{findTodoByID?.task}</Text>
+      </>
+    );
+  },
+);
 home.displayName = "home";
 
 export default home;
@@ -43,9 +51,15 @@ export default home;
 export const getServerSideProps: GetServerSideProps<{
   session: Session | null;
 }> = async (context) => {
+  const sdk = getSdk(graphQLClient);
+  const { findTodoByID } = await sdk.FindTodoById({
+    id: "322845040814588499",
+  });
+
   return {
     props: {
       session: await getSession(context),
+      findTodoByID,
     },
   };
 };
